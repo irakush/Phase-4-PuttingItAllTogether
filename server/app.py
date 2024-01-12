c679d2d43bd281a1ec54350004feab5b07ee5998
Eleanor Kelman <ekelman3@gmail.com>
2024-01-12 12:49:26 -0500

    completed lecture

--- server/app.py
+++ server/app.py
@@ -3,3 +3,113 @@ from flask import make_response, request
 from flask_restful import Api, Resource
 
 from models import db, MakeupItem, Store, StoreHasItem
+
+@app.route('/makeup_items', methods = ['GET'])
+def makeup_items():
+    makeup_items_dict = [item.to_dict(rules = ('-store_item', )) for item in MakeupItem.query.all()]
+
+    response = make_response(
+        makeup_items_dict,
+        200
+    )
+
+    return response
+
+@app.route('/stores', methods = ['GET', 'POST'])
+def stores():
+    if request.method == 'GET':
+        stores = Store.query.all()
+
+        stores_dict = [store.to_dict() for store in stores]
+
+        response = make_response(
+            stores_dict,
+            200
+        )
+    elif request.method == 'POST':
+        try:
+            form_data = request.get_json()
+
+            new_store = Store(
+                name = form_data['name']
+            )
+
+            db.session.add(new_store)
+            db.session.commit()
+
+            response = make_response(
+                new_store.to_dict(),
+                201
+            )
+        except ValueError:
+            response = make_response(
+                {"Error" : "Validations issue raised when creating Store object!"},
+                400
+            )
+
+    return response
+
+@app.route('/stores/<int:id>', methods = ['GET', 'DELETE'])
+def store_by_id(id):
+    selected_store = Store.query.filter(Store.id == id).first()
+
+    if selected_store:
+        if request.method == 'GET':
+            response = make_response(
+                selected_store.to_dict(rules = ('-item_store', )),
+                200
+            )
+        elif request.method == 'DELETE':
+            assoc_joins = StoreHasItem.query.filter(StoreHasItem.store_id == id).all()
+
+            for assoc_join in assoc_joins:
+                db.session.delete(assoc_join)
+
+            db.session.delete(selected_store)
+
+            db.session.commit()
+
+            response = make_response(
+                {},
+                204
+            )
+    else:
+        response = make_response(
+            {"Error" : "Store object not found!"},
+            404
+        )
+
+    return response
+
+@app.route('/store_has_item/<int:id>', methods = ['PATCH'])
+def store_has_item_by_id(id):
+    selected_shi = StoreHasItem.query.filter(StoreHasItem.id == id).first()
+
+    if selected_shi:
+        try:
+            form_data = request.get_json()
+
+            for attr in form_data:
+                setattr(selected_shi, attr, form_data[attr])
+
+            db.session.commit()
+
+            response = make_response(
+                selected_shi.to_dict(),
+                201
+            )
+        except ValueError:
+            response = make_response(
+                {"Error" : "Validations issue raised when creating StoreHasItem object!"},
+                400
+            )
+    else:
+        response = make_response(
+            {"Error" : "StoreHasItem object not found!"},
+            404
+        )
+
+    return response
+
+if __name__ == "__main__":
+    app.run(port = 5555, debug = True)
--- server/models.py
+++ server/models.py
@@ -5,10 +5,74 @@ from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 class MakeupItem(db.Model, SerializerMixin):
-    pass
+    __tablename__ = "makeup_items"
+
+    serialize_rules = ('-store_item.item', )
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    brand = db.Column(db.String)
+    type = db.Column(db.String)
+
+    store_item = db.relationship('StoreHasItem', back_populates = 'item')
 
 class Store(db.Model, SerializerMixin):
-    pass
+    __tablename__ = "stores"
+
+    serialize_rules = ('-item_store.store', )
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+
+    item_store = db.relationship('StoreHasItem', back_populates = 'store')
+
+    '''
+    @validates("name")
+    def validate_name(self, key, value):
+        if type(value) == str or isinstance(value, str):
+            if len(value) > 0:
+                return value
+            else:
+                raise ValueError
+        else:
+            raise ValueError
+    '''
+
+    @validates("name")
+    def validate_name(self, key, value):
+        if len(value) > 0:
+            return value
+        else:
+            raise ValueError
 
 class StoreHasItem(db.Model, SerializerMixin):
-    pass
+    __tablename__ = "store_has_item"
+
+    serialize_rules = ('-item.store_item', '-store.item_store')
+
+    id = db.Column(db.Integer, primary_key = True)
+    price = db.Column(db.Float)
+
+    item_id = db.Column(db.Integer, db.ForeignKey('makeup_items.id'))
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+
+    item = db.relationship('MakeupItem', back_populates = 'store_item')
+    store = db.relationship('Store', back_populates = 'item_store')
+
+    '''
+    @validates("price")
+    def validate_price(self, key, value):
+        if not isinstance(value, float):
+            raise ValueError
+        elif value <= 0:
+            raise ValueError
+        else:
+            return value
+    '''
+
+    @validates("price")
+    def validate_price(self, key, value):
+        if value <= 0:
+            raise ValueError
+        else:
+            return value
